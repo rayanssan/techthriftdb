de2a05a4107a5238dde2faed47b77614b113c33c
fc60282 <fc60282@alunos.ciencias.ulisboa.pt>
2025-04-21 04:23:15 +0100

    Continued implementation

--- dbCreate.js
+++ dbCreate.js
@@ -17,7 +17,7 @@ const createDatabaseIfNotExists = async () => {
     // Check if database exists, if not, create it
     const [ttdb_rows] = await connection.promise().query('SHOW DATABASES LIKE "tt_database"');
     if (ttdb_rows.length === 0) {
-        console.log('Database does not exist, creating tt_database...');
+        console.log('\x1b[33mDatabase does not exist, creating tt_database...\x1b[0m');
         await connection.promise().query('CREATE DATABASE tt_database');
 
         // Connect to the database after it is created
@@ -30,23 +30,22 @@ const createDatabaseIfNotExists = async () => {
         });
 
         // Initialize tables
-        const initSql = readFileSync(path.join(__dirname, '../database/ttDatabase.sql'), 'utf8');
+        const initSql = readFileSync(path.join(__dirname, '/database/ttDatabase.sql'), 'utf8');
         await db.promise().query(initSql);
 
         // Populate tables
-        const insertSql = readFileSync(path.join(__dirname, '../database/ttInitDatabase.sql'), 'utf8');
+        const insertSql = readFileSync(path.join(__dirname, '/database/ttInitDatabase.sql'), 'utf8');
         await db.promise().query(insertSql);
 
-        console.log('Database tt_database created and populated successfully.');
+        console.log('\x1b[32mDatabase tt_database created and populated successfully.\x1b[0m');
         db.end();
     } else {
-        console.log('Database already exists.');
+        console.log('\x1b[32mDatabase already exists.\x1b[0m');
     }
-
     // Check if replica database exists, if not, create it
     const [ttdbr_rows] = await connection.promise().query('SHOW DATABASES LIKE "tt_database_replica"');
     if (ttdbr_rows.length === 0) {
-        console.log('Replica database does not exist, creating tt_database_replica...');
+        console.log('\x1b[33mReplica database does not exist, creating tt_database_replica...\x1b[0m');
         await connection.promise().query('CREATE DATABASE tt_database_replica');
         const [tables] = await connection.promise().query("SHOW TABLES FROM tt_database");
 
@@ -56,10 +55,9 @@ const createDatabaseIfNotExists = async () => {
             await connection.promise().query(`INSERT INTO tt_database_replica.${tableName} SELECT * FROM tt_database.${tableName}`);
         }
 
-        console.log('Replica database tt_database_replica created successfully.');
-
+        console.log('\x1b[32mReplica database tt_database_replica created successfully.\x1b[0m');
     } else {
-        console.log('Replica database already exists.');
+        console.log('\x1b[32mReplica database already exists.\x1b[0m');
     }
 
     connection.end();
